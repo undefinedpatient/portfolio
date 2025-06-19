426acee2e97002dc7d4a2755e4f6cbe7935820e2
patient <undefinedpatient@gmail.com>
2025-06-19 21:53:05 +0800

    Added link <a> on click scroll to target page, Added onScroll will update the current page

--- frontend/index.js
+++ frontend/index.js
@@ -1,26 +1,46 @@
 
 console.log("index.js Loaded");
-const numOfSection = 3;
+const numOfSection = 5;
 const sectionContainerElement = document.getElementById("sectionContainer");
-
+const navBarElement = document.getElementById("navBar");
+// f(x):= -(x**2)+1
 function quadraticInterpolation(x, speed = 1, offset = 0){
     return Math.max( (-((speed*(x-offset))**2)+1), 0);
 }
 
+function scrollToSection(event){
+    const buttonList = event.target.parentElement.children;
+    // 
+    Array.from(buttonList).forEach((value, index)=>{
+            value.classList.remove("active");
+            if(value==event.target){
+                sectionContainerElement.scrollTo({
+                    top: index*sectionContainerElement.clientHeight,
+                    left: 0,
+                    behavior: "smooth"
+                });
+            }
+        }
+    )
+    event.target.classList.add("active");
+}
 sectionContainerElement.addEventListener("scroll", (event)=>{
     // let value = sectionContainerElement.scrollTo;
     let value = (numOfSection-1)*sectionContainerElement.scrollTop/(sectionContainerElement.scrollHeight-sectionContainerElement.clientHeight);
     // clearTimeout(timeout);
     try{
-        console.log("x: "+value+" y: "+Math.max( (-(value**2)+1), 0));
-        // f(x):= -(x**2)+1
         document.getElementById("homeSection").style.opacity = `${quadraticInterpolation(value, 2, 0)}`;
         document.getElementById("webDevSection").style.opacity = `${quadraticInterpolation(value, 2, 1)}`;
-        document.getElementById("contactSection").style.opacity = `${quadraticInterpolation(value, 2, 2)}`;
-        console.log(value);
+        document.getElementById("softwareDevSection").style.opacity = `${quadraticInterpolation(value, 2, 2)}`;
+        document.getElementById("artistSection").style.opacity = `${quadraticInterpolation(value, 2, 3)}`;
+        document.getElementById("contactSection").style.opacity = `${quadraticInterpolation(value, 2, 4)}`;
     } catch(error){
         console.log(error);
     }
+    Array.from(navBarElement.children).forEach((value)=>{
+        value.classList.remove("active");
+    })
+    navBarElement.children[Math.floor(value)].classList.add("active");
     
 });
 console.log("index.js Ended");
